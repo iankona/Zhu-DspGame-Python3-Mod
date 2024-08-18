9a237a3c38fcea85d980ac0e87c1c86347418530
iankona <419204845@qq.com>
2024-08-18 17:30:16 +0800

    重新添加 快速旅行 mod

---
+++ BepInEx/plugins/Python3Mod/mod/快速旅行/__init__.py
@@ -0,0 +1,33 @@
+
+from . import 星球内快速移动
+from . import 跨星系快速移动
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def Start():
+    pass
+
+def Update():
+    星球内快速移动.函数()
+    跨星系快速移动.函数()
+
+def OnGUI():
+    跨星系快速移动.ImGUI()
